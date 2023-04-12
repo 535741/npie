0bd00a8672f0e4c15e661b4505ac137ea142fdc6
535741 <130267224+535741@users.noreply.github.com>
2023-04-12 02:03:33 +0000

    Add files via upload

--- npie.c
+++ npie.c
@@ -6,7 +6,7 @@
 int menu(void){
     char input[15];
     int out;
-    printf("Pi to n=\n");
+    printf("Pi to n digits(decimal included). n = \n");
     fflush(stdout);
     fgets(input, 15, stdin);
     sscanf(input, "%i", &out);
@@ -18,14 +18,15 @@ int main(void){
     unsigned int g;
     int b;
     p = 1;
-    int in = menu();
+    int in = 1+menu();
     mpfr_prec_t x = 4*in;
     b = 10;
     mpfr_t y;
     mpfr_init2(y, x);
     mpfr_const_pi(y, MPFR_RNDF);
-    mpfr_out_str (stdout, b, in, y, MPFR_RNDF);
     printf("\n");
+    mpfr_out_str (stdout, b, in, y, MPFR_RNDF);
+    printf("\n\n");
     mpfr_clear(y);
     mpfr_mp_memory_cleanup();
 }
