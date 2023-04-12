f42e7e71a7514afd9d1e4808d0bdd38d888bca6c
535741 <130267224+535741@users.noreply.github.com>
2023-04-12 01:32:22 +0000

    Update npie.c

--- npie.c
+++ npie.c
@@ -4,11 +4,11 @@
 #include <mpfr.h>
 
 int menu(void){
-    char input[15];
+    char input[100];
     int out;
     printf("Pi to n=\n");
     fflush(stdout);
-    fgets(input, 15, stdin);
+    fgets(input, 100, stdin);
     sscanf(input, "%i", &out);
     return out;
 }
